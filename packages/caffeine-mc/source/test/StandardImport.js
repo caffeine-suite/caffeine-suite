2a1e60eaff633a2704eeda5c48f161526179f7a8
shanebdavis <shanebdavis@gmail.com>
2021-08-10 17:08:54 -0700

    patch/deps: updated JEST

--- packages/caffeine-mc/source/test/StandardImport.js
+++ packages/caffeine-mc/source/test/StandardImport.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return require('art-standard-lib').mergeWithSelf(require('art-class-system'), require('art-testbench'), require('../CaffeineMc'), {CaffeineMc: require('../CaffeineMc')});});
+Caf.defMod(module, () => {return [require('art-standard-lib'), require('art-class-system'), require('art-testbench'), require('../CaffeineMc'), {CaffeineMc: require('../CaffeineMc')}];});
 //# sourceMappingURL=StandardImport.js.map
