cb45db39f8c74f85c757f605f7cfe16c2feb4414
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2022-06-09 16:20:41 -0700

    patch/test: mocha and jest tests passing; have a known-failing test setup for dot-reference

--- packages/caffeine-script/source/test/DotReference.test.js
+++ packages/caffeine-script/source/test/DotReference.test.js
@@ -1,4 +1,4 @@
 "use strict"
 let Caf = require('caffeine-script-runtime');
-Caf.defMod(module, () => {return Caf.importInvoke(["describe", "test", "assert"], [global, require('art-standard-lib'), require('art-testbench')], (describe, test, assert) => {require('../CaffeineScript'); return describe({basics: function() {return test("big error", () => assert.eq(1, 2));}});});});
+Caf.defMod(module, () => {return Caf.importInvoke(["describe", "knownFailingEvalCafSuite"], [global, require('art-standard-lib'), require('art-testbench'), require('./TestHelpers')], (describe, knownFailingEvalCafSuite) => {require('../CaffeineScript'); return describe({basics: function() {return knownFailingEvalCafSuite("5\n3 + .", 8);}});});});
 //# sourceMappingURL=DotReference.test.js.map
